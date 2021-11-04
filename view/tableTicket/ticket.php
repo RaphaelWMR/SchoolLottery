212e43ed91451eb4962fe113dca4e3f6d7e7d3f9
Raphael <matosramosraphael@gmail.com>
2021-11-04 00:30:51 -0500

    falta boton confimar eliminar ticket

--- view/tableTicket/ticket.php
+++ view/tableTicket/ticket.php
@@ -5,16 +5,16 @@ include("/xampp/htdocs/Sorteo/controller/ticketLogic.php");
 if (isset($_POST['showTicket'])) {
     $idAlumno = $_POST['alumnoid'];
     $idMonth = $_POST['monthid'];
-    echo "A".$idAlumno . " " . $idMonth;
     $dataTicket = array();
     $dataTicket = showTicket($idAlumno, $idMonth);
-    echo print_r($dataTicket);
 }
-/*$idAlumno = $_GET['idA'];
-$idMonth = $_GET['idM'];*//*
-$dataTicket = array();
-$dataTicket = showTicket($idAlumno, $idMonth);
-echo print_r($dataTicket);*/
+$ticket_id = addDigits($dataTicket['ticket_id'], 6);
+$ticket_month = showMonth($dataTicket['month_id']);
+$alumno_ap = $dataTicket['AP'];
+$alumno_am = $dataTicket['AM'];
+$alumno_n = $dataTicket['Nombres'];
+$alumno_id = $dataTicket['# Documento'];
+$alumno_aula = $dataTicket['Aula'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -24,17 +24,45 @@ echo print_r($dataTicket);*/
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/ticket.css">
     <title>Ticket</title>
 </head>
 
 <body>
-    <h1>Ticket #<?php echo addDigits($dataTicket['ticket_id'], 6) ?></h1>
-    <p><?php echo showMonth($dataTicket['month_id']) ?></p>
-    <p><?php echo $dataTicket['AP'] ?></p>
-    <p><?php echo $dataTicket['AM'] ?></p>
-    <p><?php echo $dataTicket['Nombres'] ?></p>
-    <p><?php echo $dataTicket['# Documento'] ?></p>
-    <p><?php echo $dataTicket['Aula'] ?></p>
+    <div class="container border border-secondary rounded-3 border-3">
+        <div class="row">
+            <div class="col-3 text-center border-end border-secondary border-2">
+                <div class="img">
+                    <img src="../img/cabecera.png" class="img-fluid" alt="">
+                </div>
+                <div class="img">
+                    <img src="../img/qr_img.png" class="img-fluid" alt="">
+                </div>
+                <h2><b>TICKET N° <?php echo $ticket_id ?></b></h2>
+                <p> Mes de <?php echo $ticket_month ?><br>Sorteo en vivo 2021<br>23 de diciembre<br>05:00 p.m.</p>
+            </div>
+            <div class="col-8">
+                <h2>TICKET N°<?php echo $ticket_id ?></h2>
+                <p>Mes de <?php echo $ticket_month ?></p>
+                <hr class="new4">
+                <p>Sorteo 2021</p>
+                <p>IEP JEAN PIERRE BUCH</p>
+                <hr class="new4">
+                <p><?php echo $alumno_ap . " " . $alumno_am . "<br>" . $alumno_n ?></p>
+                <p>N° DE IDENTIFICACIÓN: <?php echo $alumno_id ?></p>
+                <p>AULA: <?php echo $alumno_aula ?></p>
+                <hr class="new4">
+                <div class=" d-flex flex-row">
+                    <div class="img text-center ">
+                        <img src="../img/cabecera.png" class="img-fluid " alt="">
+                    </div>
+                    <div class="img col-3">
+                        <img src="../img/qr_img.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
